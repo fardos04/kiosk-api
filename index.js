0a1e1b7ee5afb4f972b988e3d1bea59b0986f5f9
fardos04 <fardosabusrehan@gmail.com>
2022-03-07 09:57:30 +0200

    07032022

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+const port = 5000;
+
+const url = 'mongodb+srv://kiosk-user:v5EKQ1vGRqbVazMv@cluster0.yogor.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(url)
+.then(results=>{
+    console.log(results);
+    app.listen(port, function(){
+        console.log(`Server Is Running Via Port ${port}`);
+    })
+})
+.catch(err=>{
+    console.log(err);
+})
